033e8203b4041c8898fab4bfd11787b70670209f
Prakhar Sharma <93972115+Prakhar077@users.noreply.github.com>
2023-07-24 17:02:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {NavBar} from './myComponents/navBar';
+import {ItemList} from './myComponents/itemList';
+function App() {
+  let name1 = "Aaryan";
+  let name2 = "Arush";
+  return (
+    <>
+      <NavBar name={name1} name1={name2}/>
+      <ItemList/>
+    </>
+  );
+}
+export default App;
---
+++ src/myComponents/item.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./item.css";
+export const Item = ({ todo, onDelete }) => {
+  return (
+    <>
+      <div className="col">
+        <div id="mycard" className="card">
+          <div className="card-body">
+            <h5 id="title" className="card-title">
+              {todo.todo}
+            </h5>
+            <p id="desc" className="card-text">
+              {todo.desc}
+            </p>
+            <button
+              className="btn-sm btn-danger"
+              onClick={() => onDelete(todo.index)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
